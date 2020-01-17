c930ab0a5f4bd4e468680b2d79944920c68d4ce2
Steeve Joseph <steevejoseph@knights.ucf.edu>
2020-01-17 15:15:39 -0500

    Modify Equipment model to include qrCode

--- models/equipment.js
+++ models/equipment.js
@@ -4,6 +4,7 @@ const equipmentSchema = new mongoose.Schema({
   ID: Number,
   dateOfLastService: { type: Date, default: Date.now },
   daysOver: { type: Number, default: 0 },
+  qrCode: Object,
 });
 
 module.exports = mongoose.model('Equipment', equipmentSchema);
