c90612eb2bc3c820e6f4d2309f701a7f093379b8
Steeve Joseph <steevejoseph@knights.ucf.edu>
2020-01-19 16:25:15 -0500

    Remove signup from users route subtree (/users will need auth)

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
+
 const User = require('../models/users');
 
 router.get('/', (req, res) => {
@@ -10,34 +11,6 @@ router.get('/', (req, res) => {
     }
 
     return res.status(200).json(users);
-  });
-});
-
-router.post('/new', (req, res) => {
-  const {
-    firstName, lastName, email, totalTimeAllActivities,
-    jobTitile,
-    password,
-    username,
-    ID,
-  } = req.body;
-
-  const newUser = new User({
-    firstName,
-    lastName,
-    email,
-    totalTimeAllActivities,
-    jobTitile,
-    password,
-    username,
-    ID,
-  });
-
-  newUser.save((err, product) => {
-    if (err) {
-      return res.status(500).json(err);
-    }
-    return res.status(200).send(product);
   });
 });
 
